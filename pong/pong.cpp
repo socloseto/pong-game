c2b5676d8572a62e3fb2cdef7745ad51f26dec00
socloseto <vladik.motuzka@gmail.com>
2026-01-16 11:47:35 +0200

    feat: add basic player rectangle implementation

--- pong/pong.cpp
+++ pong/pong.cpp
@@ -7,16 +7,23 @@ int main()
     window.create(sf::VideoMode({ 800, 600 }), "PONG GAME");
     window.setFramerateLimit(60);
 
+
+    sf::RectangleShape rectanglePlayer1(sf::Vector2f(15.f, 100.f));
+    rectanglePlayer1.setFillColor(sf::Color::White);
+    rectanglePlayer1.setOrigin(rectanglePlayer1.getSize() / 2.f);
+    rectanglePlayer1.setPosition(sf::Vector2f(150.f, window.getSize().y / 2.f));
+
+ 
     while (window.isOpen())
     {
         
-        while (const std::optional event = window.pollEvent())
+        
+        while (const auto event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-
-        window.clear();
+        window.draw(rectanglePlayer1);
         window.display();
     }
 
